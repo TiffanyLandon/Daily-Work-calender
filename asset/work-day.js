82cfda83f7478e2ebbeaae2086ba0a75a57164fc
Tiffany Bryan <tiffanym.bryan@gmail.com>
2022-01-30 23:09:42 -0500

    update

--- asset/work-day.js
+++ asset/work-day.js
@@ -1,1 +1,21 @@
-$(document).ready(function)
+var todayDate = moment().format('dddd,MMM Do YYYY');
+$("#currentDay").html(todayDate);
+
+$(document).ready(function () {
+    $(".saveBtn").on("click", function () {
+        // every thing store in the html doc that matchs the id of description will have a value of the textarea
+        var info = $(this).siblings(".description").val();
+
+        // save the time to local storage and declare it here
+        var time = $(this).parent().attr("id");
+
+
+        localStorage.setItem(time, JSON.parseInt(info));
+    })
+
+    function timeTracker () {
+        var timeNow = moment().hour();
+        
+    }
+    localStorage
+})
